d6832cc2f21ce0f60a37f40155d977c7d2a70fbd
Antoni <s30687@pjwstk.edu.pl>
2026-02-23 20:27:03 +0100

    finish task1 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-01.js
+++ Node-Express-Nest/v0/solutions/task-01.js
@@ -1,7 +1,17 @@
 // Express.js app with GET /todos endpoint
 const express = require('express');
 const app = express();
-
+const PORT = 3000;
 // TODO: implement todos storage and GET /todos logic
 
-module.exports = app; 
+module.exports = app;
+
+const todos = [
+    { id: 1, title: 'title 1', completed: false },
+    { id: 2, title: 'title 2', completed: true },
+    { id: 3, title: 'title 3', completed: false },
+    { id: 4, title: 'title 4', completed: true },
+];
+
+app.get('/todos', (req, res) => { res.json(todos) });
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
