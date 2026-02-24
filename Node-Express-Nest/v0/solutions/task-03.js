06dc770fe05659d778af4eb64e8015b5f9bdc016
Antoni <s30687@pjwstk.edu.pl>
2026-02-24 12:15:00 +0100

    finish task 3 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-03.js
+++ Node-Express-Nest/v0/solutions/task-03.js
@@ -1,6 +1,23 @@
 // Express.js logging middleware for ToDo API
+const express = require('express');
+const PORT = 3000;
 function loggingMiddleware(req, res, next) {
-  // TODO: implement logging
+  console.log(req.method);
+  console.log(req.url);
+  next();
 }
 
-module.exports = loggingMiddleware; 
+module.exports = loggingMiddleware;
+const app = express();
+
+
+app.use(loggingMiddleware);
+const todos = [
+  { id: 1, title: 'title 1', completed: false },
+  { id: 2, title: 'title 2', completed: true },
+  { id: 3, title: 'title 3', completed: false },
+  { id: 4, title: 'title 4', completed: true },
+];
+
+app.get('/todos', (req, res) => { res.json(todos) });
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
