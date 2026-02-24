a6e5077237c4e987a2e322fecd24e767684558ff
Antoni <s30687@pjwstk.edu.pl>
2026-02-24 13:45:39 +0100

    finish task 8 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-08.js
+++ Node-Express-Nest/v0/solutions/task-08.js
@@ -1,3 +1,18 @@
 // Express.js static files serving for ToDo frontend
 // TODO: implement
-module.exports = {}; 
+// module.exports = {}; 
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () =>
+    console.log(`listening on ${PORT}`)
+);
