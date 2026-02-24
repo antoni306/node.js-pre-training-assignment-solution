f53dbc5fad421fbe06a1d57731a571a9d79ee8eb
Antoni <s30687@pjwstk.edu.pl>
2026-02-24 11:45:25 +0100

    cosmetic change in task 2 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-02.js
+++ Node-Express-Nest/v0/solutions/task-02.js
@@ -1,7 +1,21 @@
 // Express.js app with POST /todos endpoint
 const express = require('express');
 const app = express();
-
+const PORT = 3000;
 // TODO: implement todos storage and POST /todos logic
 
-module.exports = app; 
+module.exports = app;
+const todos = [];
+app.use(express.json());
+
+app.post('/todos', (req, res) => {
+    try {
+        const data = req.body;
+        const todo = { id: todos.length, title: data.title, completed: false };
+        todos.push(todo);
+        res.json(todo);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
