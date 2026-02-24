01d4f4548d6218a7bfa281f86a34943ec8efffe7
Antoni <s30687@pjwstk.edu.pl>
2026-02-24 12:23:02 +0100

    finish task 4 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-04.js
+++ Node-Express-Nest/v0/solutions/task-04.js
@@ -4,4 +4,21 @@ const app = express();
 
 // TODO: implement todos storage and GET /todos/:id logic
 
-module.exports = app; 
+module.exports = app;
+
+const todos = [
+    { id: 1, title: 'title 1', completed: false },
+    { id: 2, title: 'title 2', completed: true },
+    { id: 3, title: 'title 3', completed: false },
+    { id: 4, title: 'title 4', completed: true },
+];
+const PORT = 3000;
+
+
+app.get('/todos/:id', (req, res) => {
+    const id = +req.params.id
+    res.json(todos.find(todo => todo.id === id));
+});
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
