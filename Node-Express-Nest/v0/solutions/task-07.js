256a6f01d3ae7c21c37083fb46b0a1016dae1fc0
Antoni <s30687@pjwstk.edu.pl>
2026-02-24 13:31:26 +0100

    finish task 7 from Node-Express-Nest

--- Node-Express-Nest/v0/solutions/task-07.js
+++ Node-Express-Nest/v0/solutions/task-07.js
@@ -1,3 +1,15 @@
 // Express.js error handler middleware for ToDo API
 // TODO: implement
-module.exports = {}; 
+// module.exports = {};
+import express from "express"
+
+const app = express();
+const PORT = 3000;
+
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+})
+app.get('/', (req, res) => { throw new Error("Something went wrong") });
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
