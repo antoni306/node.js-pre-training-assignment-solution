85957d958d8ea64605b5408fca0794d65d5ab34d
Antoni <s30687@pjwstk.edu.pl>
2026-02-25 11:22:42 +0100

    finish task 2 from DB

---
+++ DB/solutions/task-02.sql
@@ -0,0 +1,16 @@
+
+
+
+
+insert into todos (title,description,status) values ('title','desc','active')
+
+
+select * from todos
+
+
+update todos set status = 'completed' where id =1
+
+
+delete from todos where id =1
+
+--i had a little problem with inserting values, forgot how it is done
